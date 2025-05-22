1ad12f13a1d2c1ef15523e1ad5bf966c987e4f65
Vinay Kumar Thakur <33784008+vkthakur90@users.noreply.github.com>
2025-05-22 21:50:39 +0530

    Add files via upload

--- job_submit.sh
+++ job_submit.sh
@@ -5,7 +5,6 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=01:00:00
-
 #SBATCH --job-name=dolfinx_job
 #SBATCH --output=dolfinx_%j.out
 #SBATCH --error=dolfinx_%j.err
@@ -14,3 +13,4 @@ module load OpenMPI/4.1.1-GCC-10.3.0
 
 # Run the job
 srun --mpi=pmix singularity exec ~/fenics/dolfinx.sif python3 diffusion.py
+
