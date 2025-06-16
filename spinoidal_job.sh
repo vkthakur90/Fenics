c9367939973e69bcbadb38771731f00881c234a8
Vinay Kumar Thakur <33784008+vkthakur90@users.noreply.github.com>
2025-06-16 22:15:34 +0530

    Create spinoidal_job.sh

---
+++ spinoidal_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=cpufast
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=01:00:00
+#SBATCH --job-name=dolfinx_job
+#SBATCH --output=dolfinx_%j.out
+#SBATCH --error=dolfinx_%j.err      
+
+# Run the job
+srun --mpi=pmix singularity exec ~/fenics/dolfinx.sif python3 spinoidal.py
